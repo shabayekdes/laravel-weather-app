f2bb331947e9547397aa4eab913f1cedb17c16be
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-02-22 23:51:16 +0200

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Weather app</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <script src="/js/app.js" defer></script>
+</head>
+<body class="bg-blue-500">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus accusamus fuga, porro consectetur enim recusandae asperiores maxime eos vel et nostrum, consequuntur corporis earum incidunt, iure totam repellendus. Vel, fuga.
+</body>
+</html>
